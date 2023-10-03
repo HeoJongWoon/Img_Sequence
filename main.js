299b09235e68a385eacc04daf469f88bdfdb67ce
HeoJongWoon <whddns0902@gmail.com>
2023-10-03 23:20:29 +0900

    이미지 생성 함수 개선

--- main.js
+++ main.js
@@ -15,21 +15,26 @@
 //현재수치값 / 전체수치값 * 200 (이백분율)
 
 const section = document.querySelector('section');
-
-for (let i = 0; i <= 200; i++) {
-	const img = document.createElement('img');
-	const src = document.createAttribute('src');
-	src.value = `img/pic${i}.jpg`;
-	img.setAttributeNode(src);
-	section.append(img);
-}
+const imgs = createImgs(section, 200);
+console.log(imgs);
 
 window.addEventListener('mousemove', (e) => {
 	const curPos = e.pageX;
 	const wid = window.innerWidth;
 	const percent = parseInt((curPos / wid) * 200);
 	console.log(percent);
-
 	//parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수반환
 	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
 });
+
+//인수로 갯수를 받아서 동적으로 img생성해 주는 함수
+function createImgs(target, num) {
+	for (let i = 0; i < num; i++) {
+		const img = document.createElement('img');
+		const src = document.createAttribute('src');
+		src.value = `img/pic${i}.jpg`;
+		img.setAttributeNode(src);
+		target.append(img);
+	}
+	return target.querySelectorAll('img');
+}
