c73edd65e3663e306a8e30eb2a468ac0dbe6171e
HeoJongWoon <whddns0902@gmail.com>
2023-10-03 23:19:58 +0900

    현재 이미지의 갯수에 따른 백분율 변환

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //1.이미지 동적으로 200개 생성
 //2.이미지 소스가 로딩이 될때가 에러가 발생하는 시스템 이벤트 설정
-//3.브라우저에서 마우스 움직일때마 마우스 좌표값 구하기
+//3.브라우저에서 마우스 움직일때마다 마우스 좌표값 구하기
 //4.특정 수치값을 백분율화 하는 로직처리
 //5.이미지소스가 모두 로딩되는 상태를 백분율로 변환
 
@@ -9,6 +9,10 @@
 //src속성 노드에 value = img/pic0~100.jpg
 //위의 img돔생성을 100번 반복을 돌리면서
 //append로 section 프레임안에 추가
+
+//백분율 구하는 공식
+//현재수치값 / 전체수치값 * 100 (백분율)
+//현재수치값 / 전체수치값 * 200 (이백분율)
 
 const section = document.querySelector('section');
 
@@ -19,3 +23,13 @@ for (let i = 0; i <= 200; i++) {
 	img.setAttributeNode(src);
 	section.append(img);
 }
+
+window.addEventListener('mousemove', (e) => {
+	const curPos = e.pageX;
+	const wid = window.innerWidth;
+	const percent = parseInt((curPos / wid) * 200);
+	console.log(percent);
+
+	//parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수반환
+	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
+});
